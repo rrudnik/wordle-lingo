2e915e83d3a6c825aa300c636d34df0eb7da769a
rrudnik <rudnik.raf@gmail.com>
2022-07-12 21:42:33 +0200

    .

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,15 +1,25 @@
 import React, { useEffect } from 'react'
 import useWordle from '../hooks/useWordle'
+import Grid from './Grid'
 
-export default function Wordle({solution}) {
-    const {currentGuess, handleKeyUp} = useWordle(solution)
+export default function Wordle({ solution }) {
+  const { currentGuess, handleKeyUp, guesses, isCorrect, turn } = useWordle(solution)
 
-    useEffect(() => {
-        window.addEventListener('keyup',handleKeyUp)
+  useEffect(() => {
+    window.addEventListener('keyup', handleKeyUp)
 
-        return () => window.removeEventListener('keyup',handleKeyUp)
-    },[handleKeyUp])
+    return () => window.removeEventListener('keyup', handleKeyUp)
+  }, [handleKeyUp])
+
+useEffect(()=>{
+console.log(guesses, isCorrect, turn )
+},[guesses, isCorrect, turn])
+
   return (
-    <div>current guess {currentGuess}</div>
+    <div>
+      <div>solution: {solution}</div>
+      <div>current guess: {currentGuess}</div>
+      <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+    </div>
   )
 }
--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,43 +3,83 @@ import { useState } from "react"
 const useWordle = (solution) => {
     const [turn, setTurn] = useState(0)
     const [currentGuess, setCurrentGuess] = useState('')
-    const [guesses, setGuesses] = useState([])
-    const [history, setHistory] = useState(['ninja','doors'])
+    const [guesses, setGuesses] = useState([...Array(6)])
+    const [history, setHistory] = useState(['ninja', 'doors'])
     const [isCorrect, setIsCorrect] = useState(false)
 
     const formatGuess = () => {
-        console.log({currentGuess})
+        console.log('formatting guess - ', { currentGuess })
+        let solutionArray = [...solution]
+        let formattedGuess = [...currentGuess].map((l) => {
+            return { key: l, color: 'grey' }
+        })
+
+        //find any green letters
+        formattedGuess.forEach((l, i) => {
+            if (solutionArray[i] === l.key) {
+                formattedGuess[i].color = 'green'
+                solutionArray[i] = null
+            }
+        })
+
+        //find any green letters
+        formattedGuess.forEach((l, i) => {
+            if (solutionArray.includes(l.key) && l.color !== 'green') {
+                formattedGuess[i].color = 'yellow'
+                solutionArray[solutionArray.indexOf(l.key)] = null
+            }
+        })
+
+        return formattedGuess
     }
 
-    const addNewGuess = () => {
+    const addNewGuess = (formattedGuess) => {
+        if (currentGuess === solution) {
+            setIsCorrect(true)
+        }
 
+        setGuesses((prevGuesses) => {
+            let newGuesses = [...prevGuesses]
+            newGuesses[turn] = formattedGuess
+            return newGuesses
+        })
+
+        setHistory((prevHistory) => {
+            return [...prevHistory, currentGuess]
+        })
+        
+        setTurn((prevTurn) => {
+            return prevTurn + 1
+        })
+        setCurrentGuess('')
     }
 
     const handleKeyUp = ({ key }) => {
-        if(key === 'Enter'){
+        if (key === 'Enter') {
             // only add if turn is less then 5
-            if (turn > 5){
+            if (turn > 5) {
                 console.log('you used all of your guesses')
                 return
             }
             // do not allow duplicates
-            if (history.includes(currentGuess)){
+            if (history.includes(currentGuess)) {
                 console.log('this word was already used')
                 return
             }
             // check word is 5 chars long
-            if (currentGuess.length !==5){
+            if (currentGuess.length !== 5) {
                 console.log('your guess need to be 5 char long')
                 return
             }
-            formatGuess()
+            const forrmated = formatGuess()
+            addNewGuess(forrmated)
         }
 
-        if(key ==='Backspace'){
+        if (key === 'Backspace') {
             setCurrentGuess((prev) => {
-                return prev.slice(0,-1)
+                return prev.slice(0, -1)
             })
-            return 
+            return
         }
 
         if (/^[A-Za-z]$/.test(key)) {
